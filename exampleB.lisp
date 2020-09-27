1ede34b890588a14c76c70c4cc59a86e1e8c983f
nornorm <37309186+spacedude359@users.noreply.github.com>
2020-09-27 19:32:12 +0300

    Create exampleB.lisp

---
+++ exampleB.lisp
@@ -0,0 +1,11 @@
+;; Script: exampleB.lisp
+;; Description: about module
+;; Date 27.09.2020
+
+(include BModule "exampleA.lisp")
+(BModule woops)
+
+(+ $acc $sp)
+(- $acc $sp)
+(+ $acc $sp)
+(- $acc $sp)
