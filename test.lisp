772ef2a83d15d8e3417a0b97e939d053cad856c7
nornorm <37309186+spacedude359@users.noreply.github.com>
2020-09-27 19:17:45 +0300

    Update test.lisp

--- test.lisp
+++ test.lisp
@@ -18,7 +18,7 @@
 (if $mem1 5 != (return)
 (else (output "Ha! You've been fall for that!")))
 
-;; while loops true only
+;; while loops
 (while $zero $zero == (output "Oh uh a loop!")) ; creates infinity loop next example
 
 (= $mem1 10)
@@ -27,7 +27,7 @@
 (-= $mem1 1))
 (show-reg $mem2)
 
-;; repeat same lua but it loops false only
+;; repeat
 (= $mem1 0x80)
 (repeat-until $mem1 $zero == (-= $mem1 1))
 
@@ -39,15 +39,15 @@
 (>> $acc b)
 (= $mem1 $acc)
 
-;; subroutine same like lisp it supports lambda lists
+;; subroutine
 (sub woops (a b)
 (+ $mem1 a)(* $acc b)
 (= $mem1 $acc))
 
-;; subroutine calls differently than lisp behaviour
+;; subroutine call
 (woops 2 4)
 
-;; asm same behaviour like subroutine but its bytecodes assembly language  but it creates stack calls from JAL instruction.
+;; asm
 (asm subroutine_instructions () "
  .instructions
  mov $acc, $zero, 5
@@ -56,23 +56,11 @@
 
 (sub subroutine () (subroutine_instructions))
 
-;; inline it wont create stacks by using JAL sub or asm blocks 
+;; inline
 (inline sub a ()
 (output 'Hoi!'))
 (a)
 
-;; it will looks like this
-;; CONST 1
-;; CALL 0, 1, $CONST 
-;; RET
-
-;; theres some different without inline
-;; SUB_0000:
-;; CONST 1
-;; CALL 0, 1, $CONST
-;; RET
-;; JAL SUB_0000
-;; RET
 
 ;; sooner ill add more about norn's language.
 
